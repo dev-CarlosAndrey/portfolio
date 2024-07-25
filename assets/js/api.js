9c2ba6e5bea399280151b0585d27651f8dbd80ba
dev-CarlosAndrey <andreybezerra.info@gmail.com>
2024-07-25 15:55:22 -0300

    trocando-photo

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/dev-CarlosAndrey/portfolio/main/data/profile.json?token=GHSAT0AAAAAACVJ2NLN6YO6VACJ27XV4RDEZVCTXVQ';
+    const url = 'https://raw.githubusercontent.com/dev-CarlosAndrey/portfolio/main/data/profile.json?token=GHSAT0AAAAAACVJ2NLMQ7AUXDL4CR2MXOUQZVCT5QA';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
