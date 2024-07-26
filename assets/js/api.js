0c9881253829d7025ef4300be0074a4c5281f7e3
dev-CarlosAndrey <andreybezerra.info@gmail.com>
2024-07-26 09:17:17 -0300

    alteracaoquatro

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/dev-CarlosAndrey/portfolio/main/data/profile.json?token=GHSAT0AAAAAACVJ2NLM4MOE2DNKAOQLZYJWZVDSMJA';
+    const url = 'https://raw.githubusercontent.com/dev-CarlosAndrey/portfolio/main/data/profile.json?token=GHSAT0AAAAAACVJ2NLMIC764R6JGVNAIQXQZVDSOEA';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
